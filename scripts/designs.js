392e3302fc9b70a3f1c92906e1a4bd10c52e704d
cynsdaemon <cynsdaemon@users.noreply.github.com>
2018-06-22 13:55:16 -0800

    add listener for height/width

--- scripts/designs.js
+++ scripts/designs.js
@@ -1,24 +1,41 @@
 // DOM references 
 let pixel_canvas = document.getElementById("pixel_canvas");
-let submitButton = document.getElementById("submitButton");
 let sizePicker = document.getElementById("sizePicker");
 let colorPicker = document.getElementById("colorPicker");
-let input_height = document.getElementById("input_height");
-let input_width = document.getElementById("input_width");
+let height = document.getElementById("input_height");
+let width = document.getElementById("input_width");
+let submitButton = document.querySelector('input[type="button"]');
 
 // user input can be color values
 let defaultColor = colorPicker.defaultValue; // #000000
 colorPicker.value; // returns current color
 
 
-// TODO: - 
-// colorPicker.value.addEventListener();
+// TODO: - add event listeners
+colorPicker.addEventListener("input", watchColorPicker, false);
+height.addEventListener("input", watchHeight, false);
+width.addEventListener("input", watchWidth, false);
 
-// table sizes can be dynamically set by the user.
-// set the size of the cross stitch canvas** as an N by M grid with the makeGrid() function. 
+function watchColorPicker(event){
+  let p = document.querySelectorAll("p").forEach(function(p){
+    p.style.color = event.target.value;
+  });
+}
+
+function watchHeight(event){
+  height = event.target.value;
+  console.log("This is your new height: " + height);
+
+}
+
+function watchWidth(event){
+  width = event.target.value;
+  console.log("Your new width is: " + width);
+}
+
+// TODO: set the size of the cross stitch canvas** as an N by M grid with the makeGrid() function. 
 
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid() {  
 // TODO: Make grid:
   // gridHeight
@@ -29,23 +46,31 @@ function makeGrid() {
       // add <td> to row element
 }
 
-// TODO: - draw grid (jumps thru loops)
 let gridHeight = 10;
 let gridWidth = 5;
+
 input_height.value // select height
 input_width.value // select width
 
-// add a background color to <td>
-// add row and pixel to pixel_canvas
+// TODO: add a background color to <td>
 
+// TODO: write table elements to pixel_canvas
+
+// TODO: table sizes can be dynamically set by the user.
+
+// generate table elements
 for(let h = 0; h < gridHeight; h++ ){
-  let tr = document.createElement("tr");
-  console.log(tr);
+  let row = document.createElement("tr");
+  console.log(row);
 
   for(let w = 0; w < gridWidth; w++){
-    td = tr.appendChild(document.createElement("td"));
+    data = row.appendChild(document.createElement("td"));
   }
 }
+
+pixel_canvas
+
+
 
   
 
