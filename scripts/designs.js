a602a4f8f4ae3a340eca3c3364f48a5ce05eb1d3
cynsdaemon <cynsdaemon@users.noreply.github.com>
2018-06-24 14:12:48 -0800

    add color style to table

--- scripts/designs.js
+++ scripts/designs.js
@@ -15,50 +15,64 @@ let inputWidth = document.getElementById("input_width");
   input_width.value // returns width
 */
 
-// event listeners - 
 // colorPicker:
 function watchColorPicker(event){
   let td = document.querySelectorAll("td").forEach(function(td){
-    td.style.color = event.target.value;
+    td.style.background = event.target.value;
   });
 }
 colorPicker.addEventListener("input", watchColorPicker, false);
 
-// // height:
-// function watchHeight(event){
-//   inputHeight = event.target.value;
-// }
-// inputHeight.addEventListener("input", watchHeight, false);
+// height:
+function watchHeight(event){
+  inputHeight = event.target.value;
+}
+inputHeight.addEventListener("input", watchHeight, false);
 
-// // width:
-// function watchWidth(event){
-//   inputWidth = event.target.value;
-// }
-// inputWidth.addEventListener("input", watchWidth, false);
+// width:
+function watchWidth(event){
+  inputWidth = event.target.value;
+}
+inputWidth.addEventListener("input", watchWidth, false);
 
 // Submit button
 button.addEventListener("click", function(event){
   event.preventDefault();
   let height = input_height.value;
   let width = input_width.value;
-  makeGrid(height, width); 
- }, false);
+  makeGrid(height, width);  
+}, false);
+
 
 // When size is submitted by the user, call makeGrid()
 function makeGrid(height, width) {  
-// TODO: Make grid:
   for(let h = 0; h < height; h++ ){
     let row = document.createElement("tr");
     pixel_canvas.appendChild(row);
-    
+   
     for(let w = 0; w < width; w++){
       let data = document.createElement("td");
-        row.appendChild(data);
+      row.appendChild(data);
       
     }
-  } 
+  }  
 }
 
+function removeGrid(){
+// TODO: - remove grid 
+  // if pixel_canvas has a table
+    // remove pixel_canvas child
+      // childNode.remove();
+}
+
+function removeTableClass(){
+// TODO: - style (background) attribute from td elements
+  // add event listener to td
+  // if td has bgColor
+    // when clicked, remove bgColor class
+    // when clicked again, add bgColor class
+}
+  
 
 
 // TODO: generate table elements to pixel_canvas
