8d20aab8e6242caaeaf8f5d49c316be318f67f8c
cynsdaemon <cynsdaemon@users.noreply.github.com>
2018-06-24 11:52:35 -0800

    add makeGrid function

--- scripts/designs.js
+++ scripts/designs.js
@@ -2,82 +2,65 @@
 let pixel_canvas = document.getElementById("pixel_canvas");
 let sizePicker = document.getElementById("sizePicker");
 let colorPicker = document.getElementById("colorPicker");
-let height = document.getElementById("input_height");
-let width = document.getElementById("input_width");
-let submitButton = document.querySelector('input[type="button"]');
+let button = document.querySelector('input[type="submit"]')
+let inputHeight = document.getElementById("input_height");
+let inputWidth = document.getElementById("input_width");
 
-// user input can be color values
-let defaultColor = colorPicker.defaultValue; // #000000
-colorPicker.value; // returns current color
+/* Input values
+****colorPicker:
+  let defaultColor = colorPicker.defaultValue; // #000000
+  colorPicker.value; // returns color
+***Height/Width values:
+  input_height.value // returns height
+  input_width.value // returns width
+*/
 
-
-// TODO: - add event listeners
-colorPicker.addEventListener("input", watchColorPicker, false);
-height.addEventListener("input", watchHeight, false);
-width.addEventListener("input", watchWidth, false);
-
+// event listeners - 
+// colorPicker:
 function watchColorPicker(event){
-  let p = document.querySelectorAll("p").forEach(function(p){
-    p.style.color = event.target.value;
+  let td = document.querySelectorAll("td").forEach(function(td){
+    td.style.color = event.target.value;
   });
 }
+colorPicker.addEventListener("input", watchColorPicker, false);
 
-function watchHeight(event){
-  height = event.target.value;
-  console.log("This is your new height: " + height);
+// // height:
+// function watchHeight(event){
+//   inputHeight = event.target.value;
+// }
+// inputHeight.addEventListener("input", watchHeight, false);
 
-}
+// // width:
+// function watchWidth(event){
+//   inputWidth = event.target.value;
+// }
+// inputWidth.addEventListener("input", watchWidth, false);
 
-function watchWidth(event){
-  width = event.target.value;
-  console.log("Your new width is: " + width);
-}
-
-// TODO: set the size of the cross stitch canvas** as an N by M grid with the makeGrid() function. 
+// Submit button
+button.addEventListener("click", function(event){
+  event.preventDefault();
+  let height = input_height.value;
+  let width = input_width.value;
+  makeGrid(height, width); 
+ }, false);
 
 // When size is submitted by the user, call makeGrid()
-function makeGrid() {  
+function makeGrid(height, width) {  
 // TODO: Make grid:
-  // gridHeight
-  // gridWidth
-  // for each number up to gridHeight 
-    // add row <tr> to table element  
-    // for each number up to gridWidth
-      // add <td> to row element
+  for(let h = 0; h < height; h++ ){
+    let row = document.createElement("tr");
+    pixel_canvas.appendChild(row);
+    
+    for(let w = 0; w < width; w++){
+      let data = document.createElement("td");
+        row.appendChild(data);
+      
+    }
+  } 
 }
 
-let gridHeight = 10;
-let gridWidth = 5;
 
-input_height.value // select height
-input_width.value // select width
 
-// TODO: add a background color to <td>
-
-// TODO: write table elements to pixel_canvas
-
+// TODO: generate table elements to pixel_canvas
 // TODO: table sizes can be dynamically set by the user.
-
-// generate table elements
-for(let h = 0; h < gridHeight; h++ ){
-  let row = document.createElement("tr");
-  console.log(row);
-
-  for(let w = 0; w < gridWidth; w++){
-    data = row.appendChild(document.createElement("td"));
-  }
-}
-
-pixel_canvas
-
-
-
-  
-
-
-
-
-
-
-
 
