07ec2438357f29bc389e5a360d33d7559544778a
cynsdaemon <cynsdaemon@users.noreply.github.com>
2018-06-27 11:22:06 -0800

    update event, old grid

--- scripts/designs.js
+++ scripts/designs.js
@@ -2,10 +2,7 @@
 let pixel_canvas = document.getElementById("pixel_canvas");
 let sizePicker = document.getElementById("sizePicker");
 let colorPicker = document.getElementById("colorPicker");
-let button = document.querySelector('input[type="submit"]')
-let inputHeight = document.getElementById("input_height");
-let inputWidth = document.getElementById("input_width");
-
+let button = document.querySelector('input[type="submit"]');
 /* Input values
 ****colorPicker:
   let defaultColor = colorPicker.defaultValue; // #000000
@@ -15,36 +12,37 @@ let inputWidth = document.getElementById("input_width");
   input_width.value // returns width
 */
 
-// colorPicker:
+// colorPicker
 function watchColorPicker(event){
   let td = document.querySelectorAll("td").forEach(function(td){
-    td.style.background = event.target.value;
+    td.style.background = event.target.value; // returns current color
   });
 }
 colorPicker.addEventListener("input", watchColorPicker, false);
 
-// height:
-function watchHeight(event){
-  inputHeight = event.target.value;
+// TODO: When clicked, remove style attribute
+function changeBgColor(event) {
+  let square = document.querySelector("td");
+  event.target.style.visibility = "hidden";
 }
-inputHeight.addEventListener("input", watchHeight, false);
+pixel_canvas.addEventListener("click", changeBgColor, false);  
 
-// width:
-function watchWidth(event){
-  inputWidth = event.target.value;
-}
-inputWidth.addEventListener("input", watchWidth, false);
 
-// Submit button
+// When button is clicked, call makeGrid()
 button.addEventListener("click", function(event){
   event.preventDefault();
   let height = input_height.value;
   let width = input_width.value;
-  makeGrid(height, width);  
+    // remove old grid
+    pixel_canvas.querySelectorAll("tr").forEach(function(tr){ pixel_canvas.removeChild(tr);
+
+  });
+
+  makeGrid(height, width);
+    
 }, false);
 
-
-// When size is submitted by the user, call makeGrid()
+// makeGrid 
 function makeGrid(height, width) {  
   for(let h = 0; h < height; h++ ){
     let row = document.createElement("tr");
@@ -58,23 +56,6 @@ function makeGrid(height, width) {
   }  
 }
 
-function removeGrid(){
-// TODO: - remove grid 
-  // if pixel_canvas has a table
-    // remove pixel_canvas child
-      // childNode.remove();
-}
-
-function removeTableClass(){
-// TODO: - style (background) attribute from td elements
-  // add event listener to td
-  // if td has bgColor
-    // when clicked, remove bgColor class
-    // when clicked again, add bgColor class
-}
-  
 
 
-// TODO: generate table elements to pixel_canvas
-// TODO: table sizes can be dynamically set by the user.
 
