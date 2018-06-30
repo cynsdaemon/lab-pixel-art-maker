c4f3f5fdce1277b74a66e53cb6e610dd5f1c31ed
cynsdaemon <cynsdaemon@users.noreply.github.com>
2018-06-30 11:13:07 -0800

    add fill color to square

--- scripts/designs.js
+++ scripts/designs.js
@@ -1,43 +1,28 @@
 // DOM references 
+const colorPicker = document.getElementById("colorPicker");
+const button = document.querySelector('input[type="submit"]');
 let pixel_canvas = document.getElementById("pixel_canvas");
-let sizePicker = document.getElementById("sizePicker");
-let colorPicker = document.getElementById("colorPicker");
-let button = document.querySelector('input[type="submit"]');
-/* Input values
-****colorPicker:
-  let defaultColor = colorPicker.defaultValue; // #000000
-  colorPicker.value; // returns color
-***Height/Width values:
-  input_height.value // returns height
-  input_width.value // returns width
-*/
 
-// colorPicker
-function watchColorPicker(event){
-  let td = document.querySelectorAll("td").forEach(function(td){
-    td.style.background = event.target.value; // returns current color
-  });
-}
-colorPicker.addEventListener("input", watchColorPicker, false);
+// add fill color to square
+pixel_canvas.addEventListener("click", function(event){ 
+  const color = document.querySelector('input[type="color"]').value;
+    if(event.target.tagName === "TD") {
+      event.target.style.background = color;
+    }     
+}, false);
 
-// TODO: When clicked, remove style attribute
-function changeBgColor(event) {
-  let square = document.querySelector("td");
-  event.target.style.visibility = "hidden";
-}
-pixel_canvas.addEventListener("click", changeBgColor, false);  
-
-
-// When button is clicked, call makeGrid()
+// When button is clicked,
 button.addEventListener("click", function(event){
+  // prevent default page refresh
   event.preventDefault();
+  // pull in user defined height and width,
   let height = input_height.value;
   let width = input_width.value;
     // remove old grid
     pixel_canvas.querySelectorAll("tr").forEach(function(tr){ pixel_canvas.removeChild(tr);
 
   });
-
+  // then make new grid
   makeGrid(height, width);
     
 }, false);
@@ -55,7 +40,6 @@ function makeGrid(height, width) {
     }
   }  
 }
-
 
 
 
